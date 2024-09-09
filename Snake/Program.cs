761c9d962d90a7d1b90da2e18fb8a730ea6ca00f
opilane <opilane@HP-CZC2349HTP>
2024-09-09 14:36:54 +0300

    gdg

--- Snake/Program.cs
+++ Snake/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Diagnostics.SymbolStore;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -19,6 +20,30 @@ namespace Snake
 
             Point p2 = new Point(4, 5, '#');
             p2.Draw();
+
+            List<int> numList = new List<int>();
+            numList.Add(0);
+            numList.Add(1);
+            numList.Add(2);
+            //muutujatele ligipääsu saamiseks on vaja teada nende järjekorranumbrit.
+
+            int x = numList[0];
+            int y = numList[1];
+            int z = numList[2];
+
+            foreach (int i in numList)
+            {
+                Console.WriteLine(i);
+
+            }
+
+            numList.RemoveAt(0);
+
+            List<Point> pList = new List<Point>();
+            pList.Add(p1);
+            pList.Add(p2);
+
+            //Kapseldamine - kapseldamine, isoleerimine, millegi võõra sissepiiramine, et välistada selle mõju ümbritsevale keskkonnale.
 
 
             Console.ReadLine();
