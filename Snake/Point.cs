f83d8f338d6e95acb1ea7701fcfb6d8e4489ce9e
opilane <opilane@HP-CZC2349HTP>
2024-09-09 13:36:25 +0300

    ljljk

---
+++ Snake/Point.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Snake
+{
+    //2 õppetund
+    internal class Point
+    {
+        //объединил три переменных в новый некий тип данных
+        public int x;
+        public int y;
+        public char sym;
+
+        public void Draw()
+        {
+            Console.SetCursorPosition(x, y);
+            Console.WriteLine(sym);
+        }
+    }
+}
