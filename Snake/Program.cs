8bc73ef6dedcf846ca8864d4e78413590da4e673
opilane <opilane@HP-CZC2349HTP>
2024-09-13 14:03:35 +0300

    õppetund 12

--- Snake/Program.cs
+++ Snake/Program.cs
@@ -9,13 +9,34 @@ using System.Threading.Tasks;
 
 namespace Snake
 {
-     
+
     /// 1 õppetund
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.SetWindowSize( 80, 25);
+            VerticalLine v1 = new VerticalLine(0, 10, 5, '%');
+            Draw(v1);
+
+            Point p = new Point(4, 5, '*');
+            Figure fSnake = new Snake(p, 4, Direction.RIGHT);
+            Draw(fSnake);
+            Snake snake = (Snake)fSnake;
+
+            HorizontalLine h1 = new HorizontalLine(0, 5, 6, '&');
+
+            List<Figure> figures = new List<Figure>();
+            figures.Add(fSnake);
+            figures.Add(v1);
+            figures.Add(h1);
+
+            foreach (var f in figures)
+            {
+                f.Draw();
+            }
+
+
+            Console.SetWindowSize(80, 25);
 
             //Point p1 = new Point(1, 3, '*');
             //p1.Draw();
@@ -25,61 +46,61 @@ namespace Snake
             //p2.Draw();
 
             //образовывает рамку 
-            HorizontalLine upLine = new HorizontalLine(0, 78, 0, '+');
-            HorizontalLine downLine = new HorizontalLine(0, 78, 24, '+');
-            VerticalLine leftLine = new VerticalLine(0, 24, 0, '+');
-            VerticalLine rightLine =new  VerticalLine(0, 24, 78, '+');
-            upLine.Drow();
-            downLine.Drow();
-            leftLine.Drow();
-            rightLine.Drow();
+            //HorizontalLine upLine = new HorizontalLine(0, 78, 0, '+');
+            //HorizontalLine downLine = new HorizontalLine(0, 78, 24, '+');
+            //VerticalLine leftLine = new VerticalLine(0, 24, 0, '+');
+            //VerticalLine rightLine = new VerticalLine(0, 24, 78, '+');
+            //upLine.Drow();
+            //downLine.Drow();
+            //leftLine.Drow();
+            //rightLine.Drow();
 
-            Point p = new Point(4, 5, '*');
-            p.Draw();
+            //Point p = new Point(4, 5, '*');
+            //p.Draw();
 
-            Snake snake = new Snake(p, 4, Direction.RIGHT);
-            snake.Drow();
-            snake.Move();
+            //Snake snake = new Snake(p, 4, Direction.RIGHT);
+            //snake.Drow();
+            //snake.Move();
 
-            FoodCreator foodCreator = new FoodCreator(80, 25, '$');
-            Point food = foodCreator.CreateFood();
-            food.Draw();
+            //FoodCreator foodCreator = new FoodCreator(80, 25, '$');
+            //Point food = foodCreator.CreateFood();
+            //food.Draw();
 
 
-            while(true)
-            {
-                if (snake.Eat(food))
-                {
-                    food = foodCreator.CreateFood();
-                    food.Draw();
-                    //ConsoleKeyInfo key = Console.ReadKey();
-                    //snake.HandleKey(key.Key);
-                    //ConsoleKeyInfo key = Console.ReadKey();
-                    //if (key.Key == ConsoleKey.LeftArrow)
-                    //    snake.direction = Direction.LEFT;
-                    //else if (key.Key == ConsoleKey.RightArrow)
-                    //    snake.direction = Direction.RIGHT;
-                    //else if (key.Key == ConsoleKey.DownArrow)
-                    //    snake.direction = Direction.DOWN;
-                    //else if (key.Key == ConsoleKey.UpArrow)
-                    //    snake.direction = Direction.UP;
+            //while (true)
+            //{
+            //    if (snake.Eat(food))
+            //    {
+            //        food = foodCreator.CreateFood();
+            //        food.Draw();
+            //        //ConsoleKeyInfo key = Console.ReadKey();
+            //        //snake.HandleKey(key.Key);
+            //        //ConsoleKeyInfo key = Console.ReadKey();
+            //        //if (key.Key == ConsoleKey.LeftArrow)
+            //        //    snake.direction = Direction.LEFT;
+            //        //else if (key.Key == ConsoleKey.RightArrow)
+            //        //    snake.direction = Direction.RIGHT;
+            //        //else if (key.Key == ConsoleKey.DownArrow)
+            //        //    snake.direction = Direction.DOWN;
+            //        //else if (key.Key == ConsoleKey.UpArrow)
+            //        //    snake.direction = Direction.UP;
 
-                }
-                else
-                {
-                    snake.Move();
-                }
+            //    }
+            //    else
+            //    {
+            //        snake.Move();
+            //    }
 
-                Thread.Sleep(100);
+            //    Thread.Sleep(100);
 
-                if (Console.KeyAvailable)
-                {
-                    ConsoleKeyInfo key = Console.ReadKey();
-                    snake.HandleKey(key.Key);
-                }
+            //    if (Console.KeyAvailable)
+            //    {
+            //        ConsoleKeyInfo key = Console.ReadKey();
+            //        snake.HandleKey(key.Key);
+            //    }
 
-                snake.Move(); 
-            }
+            //    snake.Move();
+            //}
 
 
 
@@ -114,6 +135,12 @@ namespace Snake
 
 
             //Console.ReadLine();
+        }
+
+        static void Draw(Figure figure)
+        {
+            figure.Draw();
+        
         }
 
 
